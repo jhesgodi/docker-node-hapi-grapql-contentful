34968a20d403bcead9de0aa7bfe77713b2cd0ccd
Jhonatan Gonzalez <jgonzalez@hugeinc.com>
2016-09-09 10:31:10 -0500

    Refactor nested promises

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -8,48 +8,77 @@ import { gelocalizeIp } from '../utils/endpoints'
 import { ARTICLE, HERO } from '../utils/contentTypes'
 
 /**
+ * Default view partials
+ * @access private
+ * @type {String}
+ */
+const views = {
+  default: 'page',
+  error: 'error'
+}
+
+/**
+ * Get geolocalization data from given ip address
+ * @param  {Number} ip ip address to be consulted
+ * @return {Promise} promise to resolve data fetched
+ */
+const loadGeoreference = (ip) => {
+  const endpoint = gelocalizeIp(ip)
+
+  return doRequest(endpoint)
+    .then((data) => data)
+}
+
+/**
+ * Get pages from CMS acording to geolocalization
+ * @param  {Object} geoRef geolocalization data
+ * @return {Promise} promise to resolve fetched pages
+ */
+const loadPages = (geoRef) => {
+  const locale = `${geoRef.locale_code}-${geoRef.country_iso_code}`
+  const provinceId = geoRef.subdivision_1_iso_code
+  const query = queries.pages(provinceId)
+
+  globals.set('locale', locale)
+
+  return graphql(schema, query)
+    .then((data) => ({ provinceId, data }))
+}
+
+/**
  * Handle requests made to route: <'/' -> Home page>
  * @param  {Object} route request parameters
  * @param  {ReplyInterface} reply reply interface to handle responses and rendering
  * @return {undefined}
  */
 export default (request, reply) => {
-  const endpoint = gelocalizeIp(request.query.ip)
+  const handleRequest = (provinceId, { data }) => {
+    const page = data.pages.find((thisPage) =>
+      thisPage.name === decodeURIComponent('Page%20A')
+    )
 
-  // TODO: Refactor this to do it by chaining Promises
-  // like: doRequest(endpoint).then(handleRequest).then(queryPage).then(reply)
-  const requestHandler = (geoRef) => {
-    const locale = `${geoRef.locale_code}-${geoRef.country_iso_code}`
-    const provinceId = geoRef.subdivision_1_iso_code
-
-    globals.set('locale', locale)
-
-    const query = queries.pages(provinceId)
-    const handler = ({ data }) => {
-      const page = data.pages.find((thisPage) =>
-        thisPage.name === decodeURIComponent('Page%20A')
-      )
-
-      if (!page) {
-        reply.view('error', {})
-      }
-
-      const modules = page.modules.filter((thisModule) =>
-        thisModule.provinces.some((thisProvince) =>
-          thisProvince.id === provinceId
-        )
-      )
-      reply.view('page', {
-        types: {
-          ARTICLE,
-          HERO
-        },
-        modules
-      })
+    if (!page) {
+      reply.view(views.error, {})
     }
 
-    graphql(schema, query).then(handler)
+    const modules = page.modules.filter((thisModule) =>
+      thisModule.provinces.some((thisProvince) =>
+        thisProvince.id === provinceId
+      )
+    )
+    const response = {
+      types: {
+        ARTICLE,
+        HERO
+      },
+      modules
+    }
+
+    reply.view(views.default, response)
   }
 
-  doRequest(endpoint).then(requestHandler)
+  loadGeoreference(request.query.ip)
+    .then(loadPages)
+    .then(handleRequest)
+    .done()
 }
